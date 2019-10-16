607f7f81bf4c946e15a26ca2e6d46833cd01b15a
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-10-16 19:57:28 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Anstoetz, Nick on 2019-10-16.
+//  Copyright © 2019 Anstoetz, Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
